59175c533fcdbb08342f0f8e9ba5fffb5fb14dfb
Brandon <brandonnlien@gmail.com>
2026-03-29 19:36:46 -0400

    Base Model
    Starting with basic CNN based on class

---
+++ src/model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.optim as optim
+
+
+class BasicCNN(nn.Module):
+    def __init__(self, num_classes=7, dropout_p=0.3):
+        super().__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout2d(p=0.25),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout2d(p=0.25),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 7 * 7, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(256, num_classes),
+        )
+
+    def forward(self, x):
+        return self.classifier(self.cnn(x))
