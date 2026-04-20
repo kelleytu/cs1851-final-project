2e96fb34384a75dac218804ae3b0657a4ba4c1ab
Russell Chiu <russellchiu@Russells-MacBook-Air-909.local>
2026-04-20 01:04:20 -0400

    plz3

--- src/model.py
+++ src/model.py
@@ -6,163 +6,9 @@ import matplotlib.pyplot as plt
 from preprocessing import FPDataLoader
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
-
-
-
-class BasicCNN(nn.Module):
-    def __init__(self, num_classes=7, dropout_p=0.3):
-        super().__init__()
-        self.loader = FPDataLoader()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout2d(p=0.25),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout2d(p=0.25),
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128 * 51 * 51, 512), # 56 for size 224x224
-            nn.ReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(256, num_classes),
-        )
-
-    def forward(self, x):
-        return self.classifier(self.cnn(x))
-
-
-
-    def train_one_epoch(self, model, X_train, y_train, optimizer, criterion, batch_size):
-
-
-        train_loader = DataLoader(
-            TensorDataset(X_train, y_train),
-            batch_size=batch_size,
-            # shuffle=True
-        )
-
-        # num_batches = (X_train.shape[0] + batch_size - 1) // batch_size
-        total_loss = 0.0
-        prob_list = []
-
-        for i, (X, y) in enumerate(train_loader):
-
-            print(f"batch {i}")
-            model.train()
-            optimizer.zero_grad()
-            logits = model(X)
-
-            # probs = torch.nn.functional.softmax(logits, dim=1)
-            # prob_list.append(probs.cpu())
-
-            loss = criterion(logits, y)
-
-            # print("backward pass")
-            loss.backward()
-            # print("optimize step")
-            optimizer.step()
-
-            total_loss += loss.item()
-
-        # prob_list = torch.cat(prob_list, dim=0)
-
-        # print(prob_list.size())
-        # flatten if needed. should be size X_train, num_classes
-
-        # return total_loss / len(train_loader), probs
-        return total_loss / len(train_loader)
-
-
-
-
-    def evaluate(self, model, X_test, y_test, criterion):
-
-
-        model.eval()
-        with torch.no_grad():
-
-
-            logits = model(X_test)
-            y_proba = torch.nn.functional.softmax(logits, dim=1)
-
-            
-            loss = criterion(logits, y_test)
-            y_pred = logits.argmax(dim=1)
-            correct = (y_pred == y_test).sum().item()
-
-            n = X_test.shape[0]
-
-        avg = "macro"
-        metrics = {
-            "accuracy": accuracy_score(y_test, y_pred),
-            "precision": precision_score(y_test, y_pred, zero_division = 0, average = avg),
-            "recall": recall_score(y_test, y_pred, zero_division = 0, average = avg),
-            "f1": f1_score(y_test, y_pred, zero_division = 0, average = avg),
-            "roc_auc": np.nan
-        }
-        metrics["roc_auc"] = roc_auc_score(y_test, y_proba, average = avg, multi_class = "ovr")
-
-        return loss.item(), correct / n, metrics, y_proba
-    
-    def run_experiment(self, model, X_train, X_test, y_train, y_test, num_epochs):
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=1e-3)
-        history = {"train_loss": [], "test_loss": [], "test_acc": []}
-
-        # later change to use preprocessing method?
-        X_train = np.transpose(X_train, (0, 3, 1, 2)) # transposed images here
-        X_test = np.transpose(X_test, (0, 3, 1, 2)) # transposed images here
-
-        X_train = torch.tensor(X_train).float()
-        X_test = torch.tensor(X_test).float()
-        y_train = torch.tensor(y_train).long()
-        y_test = torch.tensor(y_test).long()
-
-        # full_dataset = TensorDataset(X, y)
-        # train_size = int(0.8 * len(full_dataset)) # change train test split here
-        # test_size = len(full_dataset) - train_size
-        # train_dataset, test_dataset = random_split(TensorDataset(X, y), [train_size, test_size], generator=torch.Generator().manual_seed(42))     
-
-        for epoch in range(1, num_epochs + 1):
-            tr_loss = model.train_one_epoch(model, X_train, y_train, optimizer, criterion, batch_size=64) # probs for ensemble model
-            te_loss, te_acc, metrics, probs = model.evaluate(model, X_test, y_test, criterion)
-            history["train_loss"].append(tr_loss)
-            history["test_loss"].append(te_loss)
-            history["test_acc"].append(te_acc)
-            if epoch % 10 == 0:
-                print(f"  Epoch {epoch:3d} | train loss {tr_loss:.4f} | "
-                    f"test loss {te_loss:.4f} | test acc {te_acc:.3f}")
-
-        return history, metrics, probs
-
-    # def plot_results(self, model, num_epochs):
-    #     epochs  = range(1, num_epochs + 1)
-    #     colors = ["#e74c3c", "#2ecc71"]
-    #     # Loss curves
-    #     fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-    #     ax.plot(epochs, model_results["train_loss"], label="Train Loss", color=colors[0], linestyle="--", alpha=0.5)
-    #     ax.plot(epochs, hist_base_results["test_loss"],  label="Test Loss", color=colors[1], linestyle="-")
-    #     ax.set_title("Loss (— test,  -- train)")
-    #     ax.set_xlabel("Epoch")
-    #     ax.set_ylabel("Cross-Entropy Loss")
-    #     ax.legend(fontsize=9)
-    #     ax.grid(True, alpha=0.3)
-    #     plt.show()
-
-
-
+from utils import *
 import matplotlib.pyplot as plt
 # import seaborn as sns
-from typing import Tuple, Dict, Optional, Union
 
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import (
@@ -182,126 +28,250 @@ from sklearn.tree import plot_tree
 from sklearn.utils.class_weight import compute_sample_weight
 
 
-class GradientBoostingModel:
-    def __init__(
-        self,
-        max_depth: int = 3,
-        learning_rate: float = 0.1,
-        n_estimators: int = 50,
-        subsample: float = 1.0,
-        min_samples_split: int = 2,
-        min_samples_leaf: int = 1,
-        max_features: Optional[int] = None,
-        random_state: int = 42,
-    ):
+class FusionModel(nn.Module):
+    def __init__(self, num_classes=7, dropout_p=0.3):
+        super().__init__()
+        self.loader = FPDataLoader()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout2d(p=0.25),
+            # nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            # nn.ReLU(),
+            # nn.MaxPool2d(2),
+            # nn.Dropout2d(p=0.25),
+            nn.Flatten(),
+        )
+        self.tabular_head = nn.Sequential(
+            nn.Linear(3, 64),
+            nn.ReLU(),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 51 * 51, 512), 
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(256, num_classes),
+        )
 
-        self.params = {
-            "max_depth": max_depth,
-            "learning_rate": learning_rate,
-            "n_estimators": n_estimators,
-            "subsample": subsample,
-            "min_samples_split": min_samples_split,
-            "min_samples_leaf": min_samples_leaf,
-            "max_features": max_features,
-            "random_state": random_state,
-        }
+    def forward(self, x_image, x_tabular):
+        # return self.classifier(self.cnn(x))
+        cnn_out = self.cnn(x_image)
+        tabular_out = self.tabular_head(x_tabular)
+        fused = torch.cat([cnn_out, tabular_out], dim=1)
+        return self.classifier(fused)
 
-        self.model = None
-        self.feature_names = None
+    def train_one_epoch(self, model, X_img, X_tab, y, optimizer, criterion, batch_size):
 
-    def make_train_test_split(
-        self,
-        X: np.ndarray,
-        y: np.ndarray,
-        test_size: float = 0.2,
-        random_state: int = 42,
-    ):
-        self.feature_names = [f"feature_{i}" for i in range(X.shape[1])]
-        return train_test_split(X, y, test_size = test_size, random_state = random_state, stratify=y)
+        perm = torch.randperm(X_img.size(0))
+        X_img = X_img[perm]
+        y = y[perm]
+        
+        train_loader = DataLoader(
+            FusionDataset(X_img, X_tab, y),
+            batch_size=batch_size,
+            shuffle=False
+        )
+
+        # num_batches = (.shape[0] + batch_size - 1) // batch_size
+        total_loss = 0.0
+        prob_list = []
+
+        for i, (X_img, X_tab, y) in enumerate(train_loader):
+
+            print(f"batch {i}")
+            model.train()
+            optimizer.zero_grad()
+            logits = model(X_img, X_tab)
+
+            # probs = torch.nn.functional.softmax(logits, dim=1)
+            # prob_list.append(probs.cpu())
+
+            loss = criterion(logits, y)
+
+            # print("backward pass")
+            loss.backward()
+            # print("optimize step")
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        # prob_list = torch.cat(prob_list, dim=0)
+        # return total_loss / len(train_loader), probs
+        return total_loss / len(train_loader)
 
 
-    def fit(self, X_train: np.ndarray, y_train: np.ndarray, verbose: bool = True):
-        model = GradientBoostingClassifier(**self.params)
-        sample_weights = compute_sample_weight(class_weight="balanced", y=y_train)
-        model.fit(X_train, y_train, sample_weight=sample_weights)
-        self.model = model
+    def evaluate(self, model, X_img, X_tab, y, criterion):
+        model.eval()
+        with torch.no_grad():
 
-    def predict(
-        self, X: np.ndarray, return_proba: bool = False
-    ) -> Union[np.ndarray, np.ndarray]:
-        # if fitting based on training then transforming on test data
-        if return_proba:
-            preds = self.model.predict_proba(X)
-        else:
-            preds = self.model.predict(X)
-        return preds
 
-    def evaluate(self, X_test: np.ndarray, y_test: np.ndarray) -> Dict:
-        y_pred = self.predict(X_test, return_proba = False)
+            logits = model(X_img, X_tab)
+            y_proba = torch.nn.functional.softmax(logits, dim=1)
 
-        if len(np.unique(y_test)) == 2:
-            avg = "binary"
-            y_proba = self.predict(X_test, return_proba = True)[:,1]
-        else:
-            avg = "macro"
-            y_proba = self.predict(X_test, return_proba = True)
+            loss = criterion(logits, y)
+            y_pred = logits.argmax(dim=1)
+            correct = (y_pred == y).sum().item()
 
+            n = X_img.shape[0]
+
+        avg = "macro"
         metrics = {
-            "accuracy": accuracy_score(y_test, y_pred),
-            "precision": precision_score(y_test, y_pred, zero_division = 0, average = avg),
-            "recall": recall_score(y_test, y_pred, zero_division = 0, average = avg),
-            "f1": f1_score(y_test, y_pred, zero_division = 0, average = avg),
+            "accuracy": accuracy_score(y, y_pred),
+            "precision": precision_score(y, y_pred, zero_division = 0, average = avg),
+            "recall": recall_score(y, y_pred, zero_division = 0, average = avg),
+            "f1": f1_score(y, y_pred, zero_division = 0, average = avg),
+            "roc_auc": np.nan       
+            }
+        metrics["roc_auc"] = roc_auc_score(y, y_proba, average = avg, multi_class = "ovr")
+
+        return loss.item(), correct / n, metrics, y_proba
+    
+    def run_experiment(self, model, X_train_img, X_train_tab, X_test_img, X_test_tab, y_train, y_test, num_epochs):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+        history = {"train_loss": [], "test_loss": [], "test_acc": []}
+
+        # later change to use preprocessing method?
+        X_train_img = np.transpose(X_train_img, (0, 3, 1, 2)) # transposed images here
+        X_test_img = np.transpose(X_test_img, (0, 3, 1, 2)) # transposed images here
+
+        X_train_img = torch.tensor(X_train_img).float()
+        X_test_img = torch.tensor(X_test_img).float()
+        X_train_tab = torch.tensor(X_train_tab).float()
+        X_test_tab = torch.tensor(X_test_tab).float()
+
+
+        y_train = torch.tensor(y_train).long()
+        y_test = torch.tensor(y_test).long()
+
+  
+        for epoch in range(1, num_epochs + 1):
+            tr_loss = model.train_one_epoch(model, X_train_img, X_train_tab, y_train, optimizer, criterion, batch_size=64) # probs for ensemble model
+            te_loss, te_acc, metrics, probs = model.evaluate(model, X_test_img, X_test_tab, y_test, criterion)
+            history["train_loss"].append(tr_loss)
+            history["test_loss"].append(te_loss)
+            history["test_acc"].append(te_acc)
+            if epoch % 10 == 0:
+                print(f"  Epoch {epoch:3d} | train loss {tr_loss:.4f} | "
+                    f"test loss {te_loss:.4f} | test acc {te_acc:.3f}")
+
+        return history, metrics, probs
+
+
+from torchvision import models
+class ImageClassifier(nn.Module):
+    def __init__(self, num_classes=7, dropout=0.3):
+        super().__init__()
+        res_mod = models.resnet18(weights=None)
+        in_features = res_mod.fc.in_features
+        res_mod.fc = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features, num_classes)
+        )
+        self.image_classifier = res_mod
+
+    def forward(self, x):
+        return self.image_classifier(x)
+
+    def train_one_epoch(self, model, X_img, X_tab, y, optimizer, criterion, batch_size):
+
+        perm = torch.randperm(X_img.size(0))
+        X_img = X_img[perm]
+        y = y[perm]
+        
+        train_loader = DataLoader(
+            FusionDataset(X_img, X_tab, y),
+            batch_size=batch_size,
+            shuffle=False
+        )
+
+        # num_batches = (.shape[0] + batch_size - 1) // batch_size
+        total_loss = 0.0
+        prob_list = []
+
+        for i, (X_img, X_tab, y) in enumerate(train_loader):
+
+            print(f"batch {i}")
+            model.train()
+            optimizer.zero_grad()
+            logits = model(X_img, X_tab)
+
+            # probs = torch.nn.functional.softmax(logits, dim=1)
+            # prob_list.append(probs.cpu())
+
+            loss = criterion(logits, y)
+
+            # print("backward pass")
+            loss.backward()
+            # print("optimize step")
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        # prob_list = torch.cat(prob_list, dim=0)
+
+        # print(prob_list.size())
+        # flatten if needed. should be size , num_classes
+
+        # return total_loss / len(train_loader), probs
+        return total_loss / len(train_loader)
+
+
+    def evaluate(self, model, X_img, X_tab, y, criterion):
+        model.eval()
+        with torch.no_grad():
+
+
+            logits = model(X_img, X_tab)
+            y_proba = torch.nn.functional.softmax(logits, dim=1)
+
+            
+            loss = criterion(logits, y)
+            y_pred = logits.argmax(dim=1)
+            correct = (y_pred == y).sum().item()
+
+            n = X_img.shape[0]
+
+        avg = "macro"
+        metrics = {
+            "accuracy": accuracy_score(y, y_pred),
+            "precision": precision_score(y, y_pred, zero_division = 0, average = avg),
+            "recall": recall_score(y, y_pred, zero_division = 0, average = avg),
+            "f1": f1_score(y, y_pred, zero_division = 0, average = avg),
             "roc_auc": np.nan
         }
-        if len(np.unique(y_test)) == 2:
-            metrics["roc_auc"] = roc_auc_score(y_test, y_proba)
-        else:
-            metrics["roc_auc"] = roc_auc_score(y_test, y_proba, average = avg, multi_class = "ovr")
-        return metrics
+        metrics["roc_auc"] = roc_auc_score(y, y_proba, average = avg, multi_class = "ovr")
 
-    def cross_validate(
-        self,
-        X: np.ndarray,
-        y: np.ndarray,
-        cv: int = 5,
-    ) -> Dict:
-        model = GradientBoostingClassifier(**self.params)
-        pipeline = model
+        return loss.item(), correct / n, metrics, y_proba
+    
+    def run_experiment(self, model, X_img, X_test, y_train, y_test, num_epochs):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+        history = {"train_loss": [], "test_loss": [], "test_acc": []}
 
-        scoring = ["accuracy", "precision", "recall", "f1", "roc_auc_ovr"]
-        
-        results = {}
-        for metric in scoring:
-            score = cross_val_score(pipeline, X, y, cv=cv, scoring=metric)
-            results[metric] = {"mean" : np.mean(score, axis=0), 
-                               "std" : np.std(score, axis=0)}
-        return results       
+        # later change to use preprocessing method?
+        X_train = np.transpose(X_train, (0, 3, 1, 2)) # transposed images here
+        X_test = np.transpose(X_test, (0, 3, 1, 2)) # transposed images here
 
-    def tune_hyperparameters(
-        self,
-        X: np.ndarray,
-        y: np.ndarray,
-        param_grid: Dict,
-        cv: int = 3,
-        scoring: str = "f1_macro",
-    ) -> Dict:
-        pipeline = GradientBoostingClassifier(**self.params)
-        grid_search = GridSearchCV(pipeline, param_grid = param_grid, scoring = scoring, cv = cv)
-        grid_search.fit(X, y)
+        X_train = torch.tensor().float()
+        X_test = torch.tensor(X_test).float()
+        y_train = torch.tensor(y_train).long()
+        y_test = torch.tensor(y_test).long()
 
-        self.model = grid_search.best_estimator_
-        self.params.update(grid_search.best_params_)
-        
-        return { "best_params" : grid_search.best_params_,
-                "best_score" : grid_search.best_score_,
-                "cv_results" : grid_search.cv_results_
-                }
-        
-    # def plot_tree(
-    #     self, tree_index: int = 0, figsize: Tuple[int, int] = (20, 15)
-    # ) -> None:
-    #     tree = self.model.estimators_[tree_index]
-    #     plt.figure(figsize = figsize)
-    #     plot_tree(tree, feature_names = self.feature_names, filled = True)
-    #     plt.title(f"Tree {tree_index} from Gradient Boosting Ensemble")
-    #     plt.show()
+        for epoch in range(1, num_epochs + 1):
+            tr_loss = model.train_one_epoch(model, y_train, optimizer, criterion, batch_size=64) # probs for ensemble model
+            te_loss, te_acc, metrics, probs = model.evaluate(model, X_test, y_test, criterion)
+            history["train_loss"].append(tr_loss)
+            history["test_loss"].append(te_loss)
+            history["test_acc"].append(te_acc)
+            if epoch % 10 == 0:
+                print(f"  Epoch {epoch:3d} | train loss {tr_loss:.4f} | "
+                    f"test loss {te_loss:.4f} | test acc {te_acc:.3f}")
+
+        return history, metrics, probs
--- src/run_model.py
+++ src/run_model.py
@@ -1,6 +1,5 @@
 from preprocessing import FPDataLoader, preprocess_images
-from model import BasicCNN
-from model import GradientBoostingModel
+from model import *
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -27,17 +26,6 @@ loader = FPDataLoader()
 train_ids, train_images, train_labels, train_tabular, metadata = loader.get_cancer_data()
 # loader.show_sample_imgs(train_images=train_images, train_labels=train_labels)
 
-train_images = preprocess_images(train_images)
-
-EPOCHS=1
-num_classes=7
-
-tabular_model = GradientBoostingModel(
-    max_depth=3,
-    learning_rate=0.1,
-    n_estimators=500,
-    subsample=0.8
-)
 X_img_train, X_img_test, X_tab_train, X_tab_test, ids_train, ids_test, y_train, y_test = train_test_split(
     train_images, 
     train_tabular, 
@@ -47,50 +35,34 @@ X_img_train, X_img_test, X_tab_train, X_tab_test, ids_train, ids_test, y_train, 
     random_state=42, 
     stratify=train_labels)
 
-# tune_dict = tabular_model.tune_hyperparameters(X_tab_train, y_train, param_grid={
-#     'max_depth': [1, 2, 3, 4, 5],
-#     'learning_rate': [0.01, 0.05, 0.1, 0.15],
-#     'n_estimators': [100, 200, 500, 800],
-#     'subsample': [0.6, 0.8, 1.0]
-# })
-# print("\nBest hyperparameters:", tune_dict["best_params"], "\n")
+train_images = preprocess_images(train_images)
 
-tabular_model.fit(X_tab_train, y_train)
-tabular_probs = tabular_model.predict(X_tab_test, return_proba=True)
-metrics = tabular_model.evaluate(X_tab_test, y_test)
-print(pd.Series(metrics))
+EPOCHS=1
+num_classes=7
 
-print("SAVING TABULAR MODEL")
-joblib.dump(tabular_model, "tab_model.pkl")
+fusion_model = FusionModel(num_classes)
 
-model_base = BasicCNN(num_classes)
-history, metrics, cnn_probs = model_base.run_experiment(
-    model_base, 
-    X_train=X_img_train, 
-    X_test=X_img_test, 
-    y_train=y_train, 
-    y_test=y_test, 
-    num_epochs=EPOCHS)
-# probs for ensemble
+
+model_base = ImageClassifier(
+    num_classes=num_classes,
+    dropout=0.3,
+)
+history, metrics, model_probs = model_base.run_experiment(
+    X_train=X_img_train,
+    X_test=X_img_test,
+    y_train=y_train,
+    y_test=y_test,
+    num_epochs=EPOCHS
+)
 print(history)
 print(pd.Series(metrics))
 
-print("SAVING CNN MODEL")
-torch.save(model_base.state_dict(), "cnn_weights_no_erosion.pth")
-
-cnn_probs = cnn_probs.numpy()
-np.savetxt('cnn_probs.csv', cnn_probs, delimiter=',') 
-cnn_probs = np.loadtxt('cnn_probs.csv', delimiter=',')
-
-alpha = 0.6
-combined_probs = alpha * tabular_probs + (1 - alpha) * cnn_probs
-combined_preds = np.argmax(combined_probs, axis = 1)
 
 
-print("\nEnsemble Metrics:")
-print("Accuracy:", accuracy_score(y_test, combined_preds))
-print("Precision:", precision_score(y_test, combined_preds, zero_division = 0, average = "macro"))
-print("Recall:", recall_score(y_test, combined_preds, zero_division = 0, average = "macro"))
-print("F1:", f1_score(y_test, combined_preds, zero_division = 0, average = "macro"))
-print("ROC-AUC", roc_auc_score(y_test, combined_probs, average = "macro", multi_class = "ovr"))
+# print("\nEnsemble Metrics:")
+# print("Accuracy:", accuracy_score(y_test, combined_preds))
+# print("Precision:", precision_score(y_test, combined_preds, zero_division = 0, average = "macro"))
+# print("Recall:", recall_score(y_test, combined_preds, zero_division = 0, average = "macro"))
+# print("F1:", f1_score(y_test, combined_preds, zero_division = 0, average = "macro"))
+# print("ROC-AUC", roc_auc_score(y_test, combined_probs, average = "macro", multi_class = "ovr"))
 
