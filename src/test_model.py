7638479f052c248a53b6abb52c16b0cf4432b724
Russell Chiu <russell_chiu@brown.edu>
2026-05-02 18:36:30 -0400

    yay

--- src/test_model.py
+++ src/test_model.py
@@ -11,28 +11,35 @@ import pandas as pd
 num_classes=7
 tabular_dim = 3
 
+# load data
 loader = FPDataLoader()
 test1_ids, test1_images, test1_tabular = loader.get_test_cancer_data(1)
 test2_ids, test2_images, test2_tabular = loader.get_test_cancer_data(2)
 
+# concatenate datasets 1 and 2
 test_ids = np.concatenate([test1_ids, test2_ids])
 test_images = np.concatenate([test1_images, test2_images])
 test_tabular = np.concatenate([test1_tabular, test2_tabular])
 
+# preprocess and use eval transform on images
 test_images = preprocess_images(test_images)
-test_images = normalize_for_resnet(test_images)
 test_images = change_img_format(test_images)
+test_images = transform_data(test_images, train=False)
+# test_tabular = StandardScaler().fit_transform(test_tabular)
 test_tabular = torch.tensor(test_tabular).float()
 
+# load in model
 res_net_model = ResNetFusionModel(tabular_dim=tabular_dim, num_classes=num_classes)
-res_net_model.load_state_dict(torch.load("resnet_fusion_model.pt", map_location="cpu", weights_only=True))
+res_net_model.load_state_dict(torch.load("saved_models/20_aug/resnet_fusion_model.pt", map_location="cpu", weights_only=True))
 res_net_model.eval()
 
+# get predictions
 with torch.no_grad():
     logits = res_net_model(test_images, test_tabular)
     probs = torch.softmax(logits, dim=1)
     preds = torch.argmax(probs, dim=1).cpu().numpy()
 
+# submission
 submission = pd.DataFrame({"ID" : test_ids, "label" : preds})
 print(submission.head())
 print(submission["label"].value_counts())
